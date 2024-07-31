dadac69a6e9296f129b97cce151b9e0b7380c771
manojreddynandyala <129142635+manojreddynandyala@users.noreply.github.com>
2024-07-31 12:12:47 +0530

    Create 04_CFG S->0S0 program.c

---
+++ 04_CFG S->0S0 program.c
@@ -0,0 +1,29 @@
+
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+int main() {
+    char input[100] ;
+     printf("Enter a string: ");
+    scanf("%s", input);
+    int length = strlen(input);
+    int i;
+    for (i=0;i<=length;i++){
+    	if (input[i] !='0' && input[i] !='1')
+    	{
+    		printf("you entreded the input: ");
+    		break;
+		}
+    }
+    char str[100];
+    int len=strlen(input);
+    if ((input[0]=='0' && input[length-1] =='0' ) || (input[0]=='1' && input[length-1] =='1')&& input[i] != 'a' && input[i] != 'b')
+    {
+    	printf("accepted");
+	}
+	else
+	{
+		printf("not accepted");
+	}
+    return 0;
+}
